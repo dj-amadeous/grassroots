24081b35c607286accfcca66e19a69985f775e6e
evan <dj.amadeous@gmail.com>
2016-03-10 15:12:54 +0700

    management commands edit 6

--- extensions/management/commands/read_from_csv.py
+++
@@ -1,9 +0,0 @@
-from django.core.management.base import BaseCommand 
-from api import read_from_csv
-
-
-class Command(BaseCommand, change_file_path):
-    help = 'reads from csv'
-    def handle(self, **options):
-        read_from_csv()
-
--- legislators/management/commands/get_votes.py
+++ legislators/management/commands/get_votes.py
@@ -3,7 +3,9 @@ from api import create_bills
    
 class Command(BaseCommand):
     help = 'creates bills'
+    def add_arguments(self, parser):
+        parser.add_argument(input)
     def handle(self, *args, **options):
-        for i in range(20):
+        for i in range(input):
             create_bills(i)
         self.stdout.write(self.style.SUCCESS('Successfully created bills.'))
