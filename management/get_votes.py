f79e64acc6d7e98a6606e73c5b9227cf3d6d43e9
evan <dj.amadeous@gmail.com>
2016-03-10 14:41:52 +0700

    management commands edit 5

--- management/get_votes.py
+++ management/get_votes.py
@@ -1,13 +1,9 @@
 from django.core.management.base import BaseCommand
 from api import create_bills
-
-    
+   
 class Command(BaseCommand):
     help = 'creates bills'
-
-
     def handle(self, *args, **options):
         for i in range(20):
             create_bills(i)
         self.stdout.write(self.style.SUCCESS('Successfully created bills.'))
-
